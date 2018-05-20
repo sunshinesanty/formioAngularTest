54b578f3755a31b3827c3373268ddbd4d6c75c98
sunshinesanty <iyer.santoshkumar@gmail.com>
2018-05-20 20:37:18 +1000

    pointing to local

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ import { FormioAppConfig } from 'angular-formio';
 import { FormioAuthConfig } from 'angular-formio/auth';
 
 export const AppConfig: FormioAppConfig = {
-  appUrl: 'https://nnglnkqdvbdjltz.form.io',
-  apiUrl: 'https://api.form.io',
+  appUrl: 'http://localhost:3001',
+  apiUrl: 'http://localhost:3001',
   icons: 'fontawesome'
 };
 
